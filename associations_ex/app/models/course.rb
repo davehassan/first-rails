d7ef2849122f9ac8c47db819310abd063de53f35
DaveAndBobbyD <thisisnotreal@email.com>
2015-10-14 11:56:01 -0400

    Add Enrollment/Course/User associations

--- associations_ex/app/models/course.rb
+++ associations_ex/app/models/course.rb
@@ -1,2 +1,12 @@
 class Course < ActiveRecord::Base
+
+  has_many :enrollments,
+    class_name: "Enrollment",
+    foreign_key: :course_id,
+    primary_key: :id
+
+  belongs_to :professor,
+    class_name: "User",
+    foreign_key: :instructor_id,
+    primary_key: :id
 end
--- associations_ex/app/models/enrollment.rb
+++ associations_ex/app/models/enrollment.rb
@@ -1,2 +1,15 @@
 class Enrollment < ActiveRecord::Base
+
+  belongs_to :courses,
+    class_name: "Course",
+    foreign_key: :course_id,
+    primary_key: :id
+
+  belongs_to :student,
+    class_name: "User",
+    foreign_key: :student_id,
+    primary_key: :id
+
+
+
 end
